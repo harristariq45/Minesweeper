3f4f6860aed6f1018c00e6f96c1b2406c26e968a
harristariq45 <harristariq57@gmail.com>
2020-11-06 04:56:42 +0000

    flag function
    flag function with refined logic so that it works according to the rule

--- BombSquare.java
+++ BombSquare.java
@@ -6,26 +6,23 @@ public class BombSquare extends GameSquare
 {
     private GameBoard board; // Object reference to the GameBoard this square is part of.
     private boolean hasBomb; // True if this squre contains a bomb. False otherwise.
+    private boolean clicked;
+    private boolean flagged;
 
     public static final int MINE_PROBABILITY = 10;
 
     public BombSquare(int x, int y, GameBoard board)
     
     {   super(x, y, "images/blank.png");
-
+        this.clicked = false; 
+        this.flagged = false;
         this.board = board;
         hasBomb = ((int) (Math.random() * MINE_PROBABILITY)) == 0;
-
-
-
     }    
 
     @Override
     public void leftClicked(){
-
-        System.out.println(" left click ");
-        System.out.println(getXLocation() + "  left clicked   " + getYLocation());
-
+        
         if ( hasBomb == true)
         {
             System.out.println(" bomb found ");
@@ -33,50 +30,77 @@ public class BombSquare extends GameSquare
             // JOptionPane.showMessageDialog(null,
             // "YOU LOST ",
             // JOptionPane.PLAIN_MESSAGE);
-            board.dispose();
+            // board.dispose();
+            System.out.println(" left click ");
+            System.out.println(getXLocation() + "  left clicked   " + getYLocation());
         }
 
-        if ( hasBomb == false)
-        {
+        if ( hasBomb == false && this.clicked == false )
+        {   
             System.out.println(" bomb not found ");
             setImage("images/0.png");
+            System.out.println(" left click ");
+            System.out.println(getXLocation() + "  left clicked   " + getYLocation());
         }
 
+        this.clicked = true;
 
     }
 
-    public void rightClicked(){
+    public void rightClicked()
+    {   
+        // this.flagged = true;
+        // setImage("images/flag.png");
 
-        System.out.println(" right click ");
-        System.out.println(getXLocation() + "   right clicked   " + getYLocation());
+        // if (this.flagged== true)
+        // {           
+        //     setImage("images/blank.png");
+        //     this.clicked = false; 
+        //     System.out.println(" right click 2");
+        // }
+        if ( this.clicked== false){
+            if ( this.flagged == false)
+            {
+                setImage("images/flag.png");
+                this.flagged = true;
+                System.out.println(" right click 1 ");
+            }
 
-        
-
-        if (ImageIcon("images/flag.png")){
-            
-            setImage("images/blank.png");
+            else
+            {
+                setImage("images/blank.png");
+                this.flagged = false;
+                System.out.println(" right click 2 ");
+            }
         }
-        else{
-            setImage("images/flag.png");
-        }
-        
-
-        if ( hasBomb == true)
-        {
-            System.out.println(" bomb found ");
-        }
-
-        if ( hasBomb == false)
-        {
-            System.out.println(" bomb not found ");
-        }
-
-
-
     }
 
 
-    // System.out.println(getXLocation() + " before" + getYLocation());
+
+}
+        // System.out.println(" right click ");
+        // System.out.println(getXLocation() + "   right clicked   " + getYLocation());
+
+        
+
+        // if ( this.clicked== false && hasBomb == true)
+        // {
+        //     System.out.println(" bomb found ");
+        //     setImage("images/flag.png");
+        //     // this.clicked = true;
+        //     System.out.println(" right click 1 ");
+        // }
+
+        // if ( hasBomb == false && this.clicked == false)
+        // {
+        //     System.out.println(" bomb not found ");
+        //     setImage("images/flag.png");
+        //     // this.clicked = true;
+        //     System.out.println(" right click 2");
+        // }
+
+
+            // System.out.println(getXLocation() + " before" + getYLocation());
     // System.out.println(getXLocation() + " after " + getYLocation());
     // if (GameBoard.rightClicked()){
     //     System.out.println(getXLocation() + " right clicked" + getYLocation());
@@ -100,4 +124,3 @@ public class BombSquare extends GameSquare
 //         System.out.println(super.getXLocation() + "," );
 //         return super.getXLocation();
 //     }
-}
